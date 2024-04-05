f23842242b78ede26b382fa86188729d6aedab75
Sayed Rafiul Islam <sayed80@student.sust.edu>
2024-04-06 02:49:10 +0600

    image galary added

--- components/header.tsx
+++ components/header.tsx
@@ -70,7 +70,7 @@ const Header = () => {
                     </div>
                 </div>  
             </div>
-            <div className="header-psudo"/>
+            <div className="header-psudo hidden md:flex"/>
                
         </div>
      );
